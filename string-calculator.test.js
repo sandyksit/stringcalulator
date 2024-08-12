df18b935117614a2c0f830750676153b8a3044a4
Sandeep <106437097+upsahtech@users.noreply.github.com>
2024-08-12 11:30:06 +0530

    case: should return 0 for empty string

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -7,4 +7,8 @@ describe('StringCalculator', () => {
     calculator = new StringCalculator();
   });
 
+  test('should return 0 for an empty string', () => {
+    expect(calculator.add("")).toBe(0);
+  });
+
 });
