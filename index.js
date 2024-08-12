65ca3c4c4d2a8c02df2459b10e7d49c9a3b86035
Sandeep <106437097+upsahtech@users.noreply.github.com>
2024-08-12 11:43:56 +0530

    Modify content

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 class StringCalculator {
-    add(input) {
-      if (input === "") return 0;
-  
-      let delimiter = /,|\n/;
-      if (input.startsWith("//")) {
-          const delimiterEnd = input.indexOf("\n");
-          delimiter = new RegExp(input.substring(2, delimiterEnd));
-          input = input.substring(delimiterEnd + 1);
-      }
-  
-      const nums = input.split(delimiter).map(Number);
-      const negatives = nums.filter(n => n < 0);
-      
-      if (negatives.length > 0) {
-        throw new Error(`Negative numbers not allowed ${negatives.join(",")}`);
-      }
-  
-      return nums.reduce((sum, num) => sum + num, 0);
+  add(input) {
+    if (input === "") return 0;
+
+    let delimiter = /[,\n]/;
+    if (input.startsWith("//")) {
+      const parts = input.split("\n");
+      delimiter = parts[0].slice(2);
+      input = parts.slice(1).join("\n");
     }
+
+    const nums = input.split(delimiter).map(Number);
+    const negatives = nums.filter(n => n < 0);
+    
+    if (negatives.length > 0) {
+      throw new Error(`Negative numbers not allowed ${negatives.join(",")}`);
+    }
+
+    return nums.reduce((sum, num) => sum + num, 0);
   }
-  
-  module.exports = StringCalculator;
+}
+
+module.exports = StringCalculator;
