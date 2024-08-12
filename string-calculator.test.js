35f21b84a69fab8158440932b666d78f94e1c6a2
Sandeep <106437097+upsahtech@users.noreply.github.com>
2024-08-12 11:33:46 +0530

    case: should support custom delemiters

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -26,5 +26,9 @@ describe('StringCalculator', () => {
   test('should handle new lines between numbers', () => {
     expect(calculator.add("1\n2,3")).toBe(6);
   });  
+
+  test('should support custom delimiters', () => {
+    expect(calculator.add("//;\n1;2")).toBe(3);
+  });  
   
 });
